59b819861bd1b8f14c1258cbed753bad3438428e
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2025-08-30 16:22:16 +0900

    feat: implement basic function

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/suzuki-shunsuke/slog-logrus
+
+go 1.25.0
+
+require (
+	github.com/samber/slog-logrus v1.0.0
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/samber/lo v1.38.1 // indirect
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
---
+++ slogrus/log.go
@@ -0,0 +1,40 @@
+package slogrus
+
+import (
+	"log/slog"
+
+	sloglogrus "github.com/samber/slog-logrus"
+	"github.com/sirupsen/logrus"
+)
+
+func convertLevel(level logrus.Level) slog.Level {
+	switch level {
+	case logrus.TraceLevel:
+		return slog.LevelDebug
+	case logrus.DebugLevel:
+		return slog.LevelDebug
+	case logrus.InfoLevel:
+		return slog.LevelInfo
+	case logrus.WarnLevel:
+		return slog.LevelWarn
+	case logrus.ErrorLevel:
+		return slog.LevelError
+	case logrus.FatalLevel:
+		return slog.LevelError
+	case logrus.PanicLevel:
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
+
+func Convert(logE *logrus.Entry) *slog.Logger {
+	data := make([]any, 0, len(logE.Data)*2)
+	for k, v := range logE.Data {
+		data = append(data, k, v)
+	}
+	return slog.New(sloglogrus.Option{
+		Level:  convertLevel(logE.Logger.Level),
+		Logger: logE.Logger,
+	}.NewLogrusHandler()).With(data...)
+}
